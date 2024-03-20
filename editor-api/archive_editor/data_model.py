af8c9f815bc433be897bd258bf3345bb45b8d626
alexPatrie <alexanderpatrie@gmail.com>
2024-03-20 11:23:49 -0400

    added target to datamodel

--- editor-api/archive_editor/data_model.py
+++ editor-api/archive_editor/data_model.py
@@ -1,6 +1,29 @@
 from dataclasses import dataclass, asdict
-from typing import Union, Dict, List
+from typing import Union, Dict, List, Tuple
 from biosimulators_utils.sedml.data_model import SedDocument, Model, Simulation
+
+
+@dataclass
+class ParameterTarget:
+    prefix: str = None  # ie: "/sbml:sbml/sbml:model/sbml:listOfSpecies/sbml:species
+    id: str = None  # found within [@id=''], for example 'Cdh1'
+    _type: str = None  # found after id with an @, for example '@initialConcentration'
+
+    def __init__(self, value: str):
+        """Datamodel object for SEDML parameter URIs.
+
+            Parameters:
+                value:`str`: full URI from SEDML.
+        """
+        self.prefix, self.id, self._type = self.parse_uri(value)
+
+    def parse_uri(self, val: str) -> Tuple[str, str, str]:
+        parts = val.split('@')
+        print(parts)
+        prefix = parts[0].replace('[', '')
+        param_id = parts[1].replace('id=', '').replace("'", "").replace(']/', '')
+        _type = parts[-1]
+        return prefix, param_id, _type
 
 
 @dataclass
@@ -11,7 +34,7 @@ class ParameterValue:
 
 @dataclass
 class EditableSimulationParameter:
-    target: str
+    target: Union[str, ParameterTarget]
     value: Union[ParameterValue, Dict[str, Union[float, int]]]
     name: str
     target_namespaces: Dict
